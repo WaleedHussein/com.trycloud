508a84cb5677a5eacaacf61ff2e2093116d123a9
Waleed <waleedffx@gmail.com>
2022-03-27 23:26:18 -0500

    Initial Commit

---
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -0,0 +1,37 @@
+package com.trycloud.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    //1- create the object of properties
+   private static Properties properties = new Properties();
+
+    static {
+
+
+        try {
+            //2- we need to open the file in java memory: FileInputStream
+            FileInputStream file = new FileInputStream("configuration.properties");
+
+            //3- load the properties object using fileinputstream
+            properties.load(file);
+
+            //close the file
+            file.close();
+
+        } catch (IOException e) {
+            System.out.println("File not found in the ConfigurationReader class");
+        }
+
+    }
+
+
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+
+
+}
